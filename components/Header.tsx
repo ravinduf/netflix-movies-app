f364aadfcc89568a1bbedf489986cdd1caf8097c
ravinduf <ravinduf@rocketmail.com>
2022-08-14 11:20:03 +0530

    Header done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
-
+import { BellIcon, SearchIcon } from "@heroicons/react/solid"
+import Link from "next/link";
 const Header = () => {
   return (
     <header>
@@ -18,6 +19,20 @@ const Header = () => {
           <li className="headerLink">New & Popular</li>
           <li className="headerLink">My List</li>
         </ul>
+      </div>
+
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden h-6 w-6 sm:inline"/>
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6"/>
+        <Link href="/account">
+        <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
       </div>
     </header>
   )
