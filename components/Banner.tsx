5dfd763d47afae0446e65043829e9445dc0bc31b
ravinduf <ravinduf@rocketmail.com>
2022-08-17 20:18:11 +0530

    added banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,16 +1,38 @@
 import Image from "next/image"
+import { useEffect, useState } from "react"
+
+import { baseUrl } from "../constants/movie"
 import { Movie } from "../typings"
 
 interface Props {
   netflixOriginals: Movie[]
 }
 
-const Banner = ({ netflixOriginals }: Props) => (
-  <div>
+const Banner = ({ netflixOriginals }: Props) => {
+
+  // console.log(netflixOriginals, "banner");
+  
+  // const movie = netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+
+  const [movie, setMovie] = useState<Movie | null>(null)
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    )
+  }, [netflixOriginals])
+
+
+  return (
     <div>
-      {/* <Image /> */}
+      <div className="absolute top-0 left-0 h-[95vh] w-screen">
+        <Image 
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          layout="fill"
+          objectFit="cover"
+          />
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Banner
--- pages/index.tsx
+++ pages/index.tsx
@@ -28,6 +28,7 @@ const Home = ({
   romanceMovies,
   documentaries
 }: Props) => {
+
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] 
     lg:h-[140vh]">
@@ -78,7 +79,6 @@ export const getServerSideProps = async () => {
     axios(requests.fetchDocumentaries),
   ])
 
-  console.log(netflixOriginals.data)
 
   return {
     props: {
