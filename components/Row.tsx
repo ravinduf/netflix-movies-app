38b8b7b989854b5269991cdf7fddad87952ef68f
ravinduf <ravinduf@rocketmail.com>
2022-08-19 10:50:26 +0530

    hide the default scrollbar

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,28 +1,35 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid"
 import { Movie } from "../typings"
+import Thumbnail from "./Thumbnail"
 
 interface Props {
-    title: string
-    movies: Movie[]
+  title: string
+  movies: Movie[]
 }
 
 const Row = ({ title, movies }: Props) => {
-    return (
-        <div className="h-40 space-y-0.5 md:space-y-2">
-            <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5]
+  // console.log(movies)
+  return (
+    <div className="h-40 space-y-0.5 md:space-y-2">
+      <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5]
             transition duration-200 hover:text-white">
-                {title}
-            </h2>
-            <div className="group relative md:-ml-2">
-                <ChevronLeftIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9
+        {title}
+      </h2>
+      <div className="group relative md:-ml-2">
+        <ChevronLeftIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9
                 opacity-0  cursor-pointer transition hover:scale-125 group-hover:opacity-100"/>
 
-                
-                <ChevronRightIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9
-                opacity-0  cursor-pointer transition hover:scale-125 group-hover:opacity-100"/>
-            </div>
+        <div className="flex scrollbar-hide items-center space-x-0.5 
+            overflow-x-scroll md:space-x-2.5 md:p-2">
+              {movies.map((movie) => (
+                <Thumbnail key={movie.id} movie={movie}/>
+              ))}
         </div>
-    )
+        <ChevronRightIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9
+                opacity-0  cursor-pointer transition hover:scale-125 group-hover:opacity-100"/>
+      </div>
+    </div>
+  )
 }
 
 export default Row
