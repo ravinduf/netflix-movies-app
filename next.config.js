22ff08e6bf951e42c849db8850ee7315b7672b35
ravinduf <ravinduf@rocketmail.com>
2022-08-26 11:58:39 +0530

    sign in form basic ui done

--- next.config.js
+++ next.config.js
@@ -5,7 +5,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['image.tmdb.org']
+    domains: [
+      'image.tmdb.org',
+      'rb.gy',
+    ]
   }
 }
 
